c8394ad646418de28dc6dca0a8a966b871f41739
Web Master <webmaster@dc1rec.local>
2024-08-29 15:08:54 -0600

    correciones de correo v2

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -262,13 +262,13 @@ $("#form_contacto").validate({
         }
         else {
 
-            /* $('#envio_contacto').prop("disabled", true);
+             $('#envio_contacto').prop("disabled", true);
             $('#envio_contacto').html(`
                  <div class="spinner-border" style="width: 20px; height: 20px;" role="status">
                      <span class="visually-hidden">Loading...</span>
                  </div>
                  &nbsp;Enviando Datos..
-             `); */
+             `); 
 
             //* creacion de variable form data para envio de datos
             let formData = new FormData(form);
@@ -303,7 +303,7 @@ $("#form_contacto").validate({
 
                 let rutaRedireccion = setUrlBase() + respuesta.ruta;
 
-                //setTimeout("location.href='" + rutaRedireccion + "'", 2000);
+                setTimeout("location.href='" + rutaRedireccion + "'", 2000);
 
 
             }).fail(function (e) {
