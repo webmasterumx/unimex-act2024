de9efea9b020e2d4f19e6e3dd526c97873f1994f
Web Master <webmaster@dc1rec.local>
2024-08-29 10:52:05 -0600

    se agregaron utm si carrera v1

--- resources/views/include/redirecciones/outOfertaAcademica.blade.php
+++ resources/views/include/redirecciones/outOfertaAcademica.blade.php
@@ -2,12 +2,12 @@
     function calculadoraHeader() {
         let rutaRedireccionCalculadora = setUrlBase() +
             `calcula-tu-cuota?utm_source=Website+Metro&utm_medium=Organico&utm_campaign=Home+header&utm_term=Calculadora&utm_content=Menu+Calculadora`;
-        window.open("{{ route('calcula_tu_cuota') }}", '_blank');
+        window.open(rutaRedireccionCalculadora, '_blank');
     }
 
     function preinscripcionHeader() {
         let rutaRedireccionPreinscripcion = setUrlBase() +
             `App/Preinscripcion-online?utm_source=Website+Metro&utm_medium=Organico&utm_campaign=Home+header&utm_term=Preinscrip&utm_content=Menu+Preinscrip`;
-        window.open("{{ route('preinscripcion.linea') }}", '_blank');
+        window.open(rutaRedireccionPreinscripcion, '_blank');
     }
 </script>
