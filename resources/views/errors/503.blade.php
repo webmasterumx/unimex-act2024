98d3c24a44ec12f220899782469789abda43a263
Web Master <webmaster@dc1rec.local>
2024-11-14 09:51:44 -0600

    se modifico la vista de mantenimento

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,26 +1,29 @@
 @extends('layouts.layoutError')
 
 @section('titulo')
-    Pagina no encontrada
+    Sitio en mantenimiento
 @endsection
 
 @section('content')
     <section class="container-fluid">
         <div class="row">
-            <div class="col-5 text-center">
-                <img src="{{ asset('assets/img/extras/acercade/principal.png') }}" alt="">
+            <div class="col-12 text-center">
+                <img class="img-fluid" width="150px" src="{{ asset('assets/img/extras/acercade/principal.png') }}" alt="">
             </div>
-            <div class="col-7 text-center">
-                <p style="font-size: 150px; margin-top: 4rem !important">
-                    404  
+            <div class="col-12 col-md-12 col-lg-6 col-xl-6 text-center">
+                <img src="{{ asset('assets/img/leon_mantenimiento_2.png') }}" width="400px" class="img-fluid m-auto" alt="">
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-6 text-center">
+                <p style="font-size: 150px;">
+                    503
                 </p>
                 <p class="fs-1">
                     UPS! LO SENTIMOS
                 </p>
                 <p class="fs-3">
-                    Página no encontrada
+                    Sitio en mantenimiento
                 </p>
-                <a href="{{ route('inicio') }}" class="btn btn-primary">Volver al Inicio</a>
+                {{-- <a href="{{ route('inicio') }}" class="btn btn-primary">Recargar la página</a> --}}
             </div>
         </div>
     </section>
